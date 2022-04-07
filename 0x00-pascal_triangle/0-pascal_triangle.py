b070dd1d76b6de25e22ff82c2bfc93d06ca883d2
Kevin Ng'ang'a <knganga28@gmail.com>
2022-04-07 16:01:38 -0700

    fixed error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,9 +5,9 @@ def pascal_triangle(n):
     if n <= 0:
         return list()
     else:
-        previous = [0] + pascal(n-1) + [0]
-        new_row = [previous[i] + previous[i+1] for i in range(len(previous)-1)]
-        return new_row
-    
-    for n in range(5):
-        print(pascal(n))  
+        for i in range(n):
+            # adjust space
+            print(' '*(n-i), end='')
+            
+            # compute power of 11
+            print(' '.join(map(str, str(11**i))))
